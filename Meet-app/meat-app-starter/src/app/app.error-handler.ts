e096921146e7bfb62f6388703d13015df5eeb752
LeonardoLT <leoltavares19@gmail.com>
2022-03-10 15:22:24 -0300

    Correção na emissão de mensagem de erro no arquivo error-handler

--- Meet-app/meat-app-starter/src/app/app.error-handler.ts
+++ Meet-app/meat-app-starter/src/app/app.error-handler.ts
@@ -5,13 +5,13 @@ import 'rxjs/add/Observable/throw';
 export class ErrorHandler{
     static handleError(error: Response | any){
         let errorMessage: string
-        console.log(errorMessage)
 
         if(error instanceof Response){
             errorMessage = `Erro ${error.status} ao acessar a URL ${error.url} - ${error.statusText}`
         }else{
             errorMessage = error.toString()
         }
+        console.log(errorMessage)
 
         return Observable.throw(errorMessage)
         
