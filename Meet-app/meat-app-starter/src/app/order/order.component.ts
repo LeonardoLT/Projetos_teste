1fe3e003eaeb4fe023982e59b7ca1b2230a79ac7
LeonardoLT <leoltavares19@gmail.com>
2022-03-17 19:30:41 -0300

    Inclusão do botão de radio para formas de pagamento

--- Meet-app/meat-app-starter/src/app/order/order.component.ts
+++ Meet-app/meat-app-starter/src/app/order/order.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { ShoppingCartService } from 'app/restaurant-detail/shopping-cart/shopping-cart.service';
+import { RadioOption } from 'app/shared/radio/radio-option.model';
 
 @Component({
   selector: 'mt-order',
@@ -10,6 +11,11 @@ export class OrderComponent implements OnInit {
 
   constructor(private shoppingCartService: ShoppingCartService) { }
 
+  paymentOptions: RadioOption[] = [
+    {label: 'Dinheiro', value: 'MON'},
+    {label: 'Cartão de Débito', value: 'DEB'},
+    {label: 'Vale Refeição', value: 'REF'}
+  ];
   orderItems: CartItem[];
   quantity: number = 1;
 
