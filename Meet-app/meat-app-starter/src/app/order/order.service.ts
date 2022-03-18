34591c9855d4ddb77e8faed6d462086cf6e21ae9
LeonardoLT <leoltavares19@gmail.com>
2022-03-18 18:44:06 -0300

    Implementação da finalização da compra

--- Meet-app/meat-app-starter/src/app/order/order.service.ts
+++ Meet-app/meat-app-starter/src/app/order/order.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from "@angular/core";
+import { Headers, Http, RequestOptions } from "@angular/http";
+import { MEAT_API } from "app/app.api";
+import { ErrorHandler } from "app/app.error-handler";
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
 import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { Observable } from "rxjs/Observable";
+import { Order } from "./order.model";
 
 @Injectable()
 export class OrderService{
 
-    constructor(private shoppingCartService: ShoppingCartService){} 
+    constructor(private shoppingCartService: ShoppingCartService, private http: Http){} 
   
     cartItems(): CartItem[]{
         return this.shoppingCartService.items;
@@ -25,6 +30,19 @@ export class OrderService{
 
     removeItem(item: CartItem){
         this.shoppingCartService.removeItem(item);
+    }
+
+    clear(){
+        this.shoppingCartService.clear();
+    }
+
+    checkOrder(order: Order): Observable<string>{
+        const headers = new Headers
+        headers.append('Content-type', 'application/json');
+        return this.http.post(`${MEAT_API}/orders`, JSON.stringify(order), new RequestOptions({headers: headers}))
+            .map(response => response.json())
+            .map(order => order.id)
+            .catch(ErrorHandler.handleError)
     }
 
 }
