f68c3ce4257bb33d96c8ef9d199faa12b9142d12
LeonardoLT <leoltavares19@gmail.com>
2022-03-28 17:23:10 -0300

    Ajustes de rota e http para prod

--- Meet-app/meat-app-starter/src/app/app.api.ts
+++ Meet-app/meat-app-starter/src/app/app.api.ts
@@ -1,1 +1,2 @@
 export const MEAT_API = 'http://localhost:3000'
+//export const MEAT_API = environment.api
--- Meet-app/meat-app-starter/src/app/app.module.ts
+++ Meet-app/meat-app-starter/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 
 @NgModule({
@@ -44,7 +45,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}, {provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
