931070a66fc08bd48fa0877fcf9cc482091d5e25
LeonardoLT <leoltavares19@gmail.com>
2022-04-05 19:49:59 -0300

    Criando autenticação para a rota de login

--- Meet-app/meat-app-starter/backend/server.ts
+++ Meet-app/meat-app-starter/backend/server.ts
@@ -3,6 +3,7 @@ import { Express } from 'express'
 
 import * as fs from 'fs'
 import * as https from 'https'
+import { handleAuthentication } from './auth'
 
 const server: Express = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -15,7 +16,8 @@ server.use(middlewares)
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
 
-//
+//middleware para login
+server.post('/login', handleAuthentication)
 
 // Use default router
 server.use(router)
