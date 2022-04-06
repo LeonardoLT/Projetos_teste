21b3e49eb2602d85e2d11edb4a09e92ab6640815
LeonardoLT <leoltavares19@gmail.com>
2022-04-06 17:53:42 -0300

    Implementação do canLoad

--- Meet-app/meat-app-starter/src/app/security/login/login.component.ts
+++ Meet-app/meat-app-starter/src/app/security/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NotificationService } from 'app/shared/messages/notification.service';
 import { LoginService } from './login.service';
 import { User } from "./user.model";
@@ -12,22 +13,29 @@ import { User } from "./user.model";
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup
+  navigateTo: string
 
   constructor(private formbuilder: FormBuilder,
               private loginService: LoginService,
-              private notificationService: NotificationService) { }
+              private notificationService: NotificationService,
+              private actvatedRoute: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     this.loginForm = this.formbuilder.group({
       email: this.formbuilder.control('', [Validators.required, Validators.email]),
       password: this.formbuilder.control('', [Validators.required])
     })
+    this.navigateTo = this.actvatedRoute.snapshot.params['to'] || '/'
   }
 
   login(){
     this.loginService.login(this.loginForm.value.email, this.loginForm.value.password)
-      .subscribe(user => this.notificationService.notify(`Bem vindo(a) ${user.name}`),
-      response => this.notificationService.notify(response.error.message))
+                            .subscribe(user => this.notificationService.notify(`Bem vindo(a) ${user.name}`),
+                                       response => this.notificationService.notify(response.error.message),
+                                       () => {
+                                         this.router.navigate([this.navigateTo])
+                                       })
   }
 
 }
--- Meet-app/meat-app-starter/src/app/shared/shared.module.ts
+++ Meet-app/meat-app-starter/src/app/shared/shared.module.ts
@@ -10,6 +10,7 @@ import { RatingComponent } from "./rating/rating.component";
 import { SnackbarComponent } from './messages/snackbar/snackbar.component';
 import { NotificationService } from "./messages/notification.service";
 import { LoginService } from "app/security/login/login.service";
+import { LoggedInGuard } from "app/security/loggedin.guard";
 
 @NgModule({
     declarations:[InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
@@ -21,7 +22,7 @@ export class SharedModule{
     static forRoot(): ModuleWithProviders{
         return {
             ngModule: SharedModule,
-            providers: [RestaurantsService, ShoppingCartService, OrderService, NotificationService, LoginService]
+            providers: [RestaurantsService, ShoppingCartService, OrderService, NotificationService, LoginService, LoggedInGuard]
         }
     }
 }
